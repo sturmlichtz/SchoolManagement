880f6a424de891fd74e88a53e6a87c46ee0e048d
sturmlichtz <backend.roshan@gmail.com>
2023-02-02 01:23:54 +0530

    Add project files.

---
+++ Models/SchoolDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagement.Models
+{
+    public class SchoolDbContext: DbContext
+    {
+        public  DbSet<Department> Departments { set; get; }
+        public DbSet<Student> Students { set; get; }
+        public DbSet<Course> Courses { set; get; }
+        public DbSet<Semester> Semesters { get; set; }
+    }
+}
